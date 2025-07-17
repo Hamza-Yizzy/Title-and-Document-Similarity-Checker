edd7011d9a47847bb95ac2b2ce376187b4a2f1ce
Hamza-Yizzy <izzoG119@gmail.com>
2025-07-17 11:26:18 +0300

    Last Updated !

--- Similarity_Check/Similarity_Check/settings.py
+++ Similarity_Check/Similarity_Check/settings.py
@@ -139,3 +139,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Session Settings
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True        # End session on browser/tab close
+SESSION_COOKIE_AGE = 30  # 30 seconds
+SESSION_SAVE_EVERY_REQUEST = True             # Reset timer on each request
